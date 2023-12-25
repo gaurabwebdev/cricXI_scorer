3418125b21bd07ac3822507273826d1ab3a77177
Simi Sur <gaurabbose40@gmail.com>
2023-12-25 12:41:22 +0600

    adjust css styles

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const teamOne = [];
 const teamTwo = [];
 
+// P l A Y E R      A D D        F U N C T I O N
+
 const playerAdd = () => {
   const addBtn = document.querySelectorAll(
     ".player-registration-container .player-add-btn"
@@ -9,26 +11,31 @@ const playerAdd = () => {
   addBtn.forEach((x) => {
     x.addEventListener("click", (e) => {
       const clickedBtn = e.target;
+      const playerRegisterContainer = clickedBtn.parentElement.parentElement;
       const plyerNameInput =
         clickedBtn.parentElement.querySelector(".player-name-input");
+      const playerList =
+        playerRegisterContainer.querySelector(".player-list ol");
       if (plyerNameInput.value) {
         const currentPlayerInputName = plyerNameInput.value;
         if (
-          clickedBtn.parentElement.parentElement.querySelector(".team-name")
-            .textContent === "Team One"
+          playerRegisterContainer.querySelector(".team-name").textContent ===
+          "Team One"
         ) {
-          teamOne.push(currentPlayerInputName);
-          console.log(true);
-          console.log(currentPlayerInputName);
+          const newPlayer = document.createElement("li");
+          newPlayer.textContent = plyerNameInput.value;
+          playerList.appendChild(newPlayer);
+          teamOne.push(plyerNameInput.value);
           console.log(teamOne);
           plyerNameInput.value = "";
         } else if (
           clickedBtn.parentElement.parentElement.querySelector(".team-name")
             .textContent === "Team Two"
         ) {
-          teamTwo.push(currentPlayerInputName);
-          console.log(false);
-          console.log(currentPlayerInputName);
+          const newPlayer = document.createElement("li");
+          newPlayer.textContent = plyerNameInput.value;
+          playerList.appendChild(newPlayer);
+          teamOne.push(plyerNameInput.value);
           console.log(teamTwo);
           plyerNameInput.value = "";
         }
