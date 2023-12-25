be1663662a2c82d7e82bb2b3c89ed71483f61993
Simi Sur <gaurabbose40@gmail.com>
2023-12-25 12:03:31 +0600

    player name receive name function created

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
+const teamOne = [];
+const teamTwo = [];
+
 const playerAdd = () => {
-  const teamOne = [];
   const addBtn = document.querySelectorAll(
-    ".player-registraton-container .player-add-btn"
+    ".player-registration-container .player-add-btn"
   );
 
   addBtn.forEach((x) => {
     x.addEventListener("click", (e) => {
       const clickedBtn = e.target;
-      console.log(clickedBtn);
+      const plyerNameInput =
+        clickedBtn.parentElement.querySelector(".player-name-input");
+      if (plyerNameInput.value) {
+        const currentPlayerInputName = plyerNameInput.value;
+        if (
+          clickedBtn.parentElement.parentElement.querySelector(".team-name")
+            .textContent === "Team One"
+        ) {
+          teamOne.push(currentPlayerInputName);
+          console.log(true);
+          console.log(currentPlayerInputName);
+          console.log(teamOne);
+          plyerNameInput.value = "";
+        } else if (
+          clickedBtn.parentElement.parentElement.querySelector(".team-name")
+            .textContent === "Team Two"
+        ) {
+          teamTwo.push(currentPlayerInputName);
+          console.log(false);
+          console.log(currentPlayerInputName);
+          console.log(teamTwo);
+          plyerNameInput.value = "";
+        }
+      } else {
+        alert("Please Enter Your Player Name");
+      }
     });
   });
 };
